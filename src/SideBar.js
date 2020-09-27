c001e2c2f7881672aeb8a1aef5bc2e324cf1e6f7
Ronald Francois <rfrancois@Ronalds-MacBook-Pro.local>
2020-09-27 00:20:28 -0400

    sidebar Icons & firebase config

--- src/SideBar.js
+++ src/SideBar.js
@@ -2,6 +2,17 @@ import React from "react";
 import "./Sidebar.css";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function SideBar() {
   return (
@@ -16,6 +27,22 @@ function SideBar() {
         </div>
         <CreateIcon />
       </div>
+      <SidebarOption Icon={InsertCommentIcon} title="Theads" />
+      <SidebarOption Icon={InboxIcon} title="Mention & Reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel bookmark" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File browser" />
+      <SidebarOption title="the boom boom room" />
+
+      <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+      <hr />
+      <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+      <hr />
+      <SidebarOption Icon={AddIcon} title="Add Channels" />
+      {/* connect to db and list all the channels */}
+      {/* <sidebarOption..... */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBEPLbUCpSCyiV3GwMHhHCa4MX3YfGXqo",
+  authDomain: "slack-clone-rf.firebaseapp.com",
+  databaseURL: "https://slack-clone-rf.firebaseio.com",
+  projectId: "slack-clone-rf",
+  storageBucket: "slack-clone-rf.appspot.com",
+  messagingSenderId: "733499271650",
+  appId: "1:733499271650:web:9dbcc92ac0a6c1e131b1bb",
+};
