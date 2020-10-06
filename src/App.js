78576dc7f29f251aaee4c5ac86684f31c283115a
Ronald Francois <rfrancois@Ronalds-MacBook-Pro.local>
2020-10-06 14:13:42 -0400

    firebase and chat

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import SideBar from "./SideBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Chat from "./Chat.js";
 
 function App() {
   return (
     <div className="App">
-      <p>Hi Ronald !</p>
+      <Router>
+        <p>Hi Ronald !</p>
+        <Header />
 
-      <Header />
-      <div className="app__body">
-        <SideBar />
-      </div>
+        <div className="app__body">
+          <SideBar />
 
-      {/* // react-router ---> chat screen */}
+          <Switch>
+            <Route path="/room/:roomId">
+              <h1>Chat Screen</h1>
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1> Welcome the pathhouse!!</h1>
+            </Route>
+          </Switch>
+          {/* // react-router ---> chat screen */}
+        </div>
+      </Router>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBBEPLbUCpSCyiV3GwMHhHCa4MX3YfGXqo",
   authDomain: "slack-clone-rf.firebaseapp.com",
@@ -7,3 +9,14 @@ const firebaseConfig = {
   messagingSenderId: "733499271650",
   appId: "1:733499271650:web:9dbcc92ac0a6c1e131b1bb",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+// const googleProvider = new firebase.auth.GoogleAuthProvider();
+// const emailProvider = new firebase.auth.EmailAuthProvider();
+// const [googleProvider, emailProvider] = provider;
+
+export { auth, provider };
+export default db;
